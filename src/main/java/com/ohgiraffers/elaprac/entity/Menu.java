56077d4e3b8f42ce108bb737ae066a681942f386
Wrinkk <wjdxodndjaak@naver.com>
2024-05-08 10:58:14 +0900

    first commit

---
+++ src/main/java/com/ohgiraffers/elaprac/entity/Menu.java
@@ -0,0 +1,29 @@
+package com.ohgiraffers.elaprac.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "tbl_menu")
+@Data
+public class Menu {
+
+    @Id
+    @Column(name = "menu_code")
+    private int menuCode;
+
+    @Column(name = "menu_name")
+    private String menuName;
+
+    @Column(name = "menu_price")
+    private int menuPrice;
+
+    @Column(name = "category_code")
+    private int categoryCode;
+
+    @Column(name = "order_code")
+    private String orderableStatus;
+}
